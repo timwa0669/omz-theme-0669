e163bdacc16297b4e34e3fe264f65fb8774d2ada
timwa0669 <timwa0669@hotmail.com>
2023-06-27 22:19:50 +0800

    Add install script

---
+++ install.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+
+cd ${HOME}/.oh-my-zsh/custom
+rm -rf omz-theme-0669 themes/0669.zsh-theme
+git clone https://github.com/timwa0669/omz-theme-0669
+cd themes
+ln -sfn ../omz-theme-0669/0669.zsh-theme 0669.zsh-theme
+sed -i -E 's/ZSH_THEME=\".*/ZSH_THEME=\"0669\"/g' ${HOME}/.zshrc
+git config --global --add oh-my-zsh.hide-dirty 1
