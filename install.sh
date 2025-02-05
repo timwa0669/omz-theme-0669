f86b70657512982c48ff94910da6bf445763b858
Tim Wang <timwa0669@hotmail.com>
2025-02-05 15:09:55 +0800

    Update install.sh
    Add path-related protection

--- install.sh
+++ install.sh
@@ -1,10 +1,6 @@
 #!/bin/sh
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
-cd ${HOME}/.oh-my-zsh/custom
-rm -rf omz-theme-0669 themes/0669.zsh-theme
-git clone https://github.com/timwa0669/omz-theme-0669
-cd themes
-ln -sfn ../omz-theme-0669/0669.zsh-theme 0669.zsh-theme
-sed -i -E 's/ZSH_THEME=\".*/ZSH_THEME=\"0669\"/g' ${HOME}/.zshrc
-git config --global --add oh-my-zsh.hide-dirty 1
+rm -rf ${HOME}/.oh-my-zsh/custom/omz-theme-0669 ${HOME}/.oh-my-zsh/custom/themes/0669.zsh-theme
+cd ${HOME}/.oh-my-zsh/custom && git clone https://github.com/timwa0669/omz-theme-0669
+cd ${HOME}/.oh-my-zsh/custom/themes && ln -sfn ../omz-theme-0669/0669.zsh-theme 0669.zsh-theme && sed -i -E 's/ZSH_THEME=\".*/ZSH_THEME=\"0669\"/g' ${HOME}/.zshrc && git config --global --add oh-my-zsh.hide-dirty 1
